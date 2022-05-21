12e559ae07c8951450c542d70c9072297f305e18
crypte-1 <renukavutukuri1@gmail.com>
2022-05-21 22:11:29 +0530

    added constants.js

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xfb78f326eA59B95D753A44b8a08482C76EA6f59c";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
