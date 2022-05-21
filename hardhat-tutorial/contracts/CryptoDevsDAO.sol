4d568198b6f9af6935d9b6ab5644863cc80e2e18
crypte-1 <renukavutukuri1@gmail.com>
2022-05-21 21:05:39 +0530

    Added modifiers nftHolderOnly , activeProposalOnly , inactiveProposalOnly

--- hardhat-tutorial/contracts/CryptoDevsDAO.sol
+++ hardhat-tutorial/contracts/CryptoDevsDAO.sol
@@ -72,5 +72,40 @@ contract CryptoDevsDAO is Ownable {
     mapping(uint256 => Proposal) public proposals;
     // Number of proposals that have been created
     uint256 public numProposals;
+
+
+
+    
+    // Create a modifier which only allows a function to be
+    // called by someone who owns at least 1 CryptoDevsNFT
+    modifier nftHolderOnly() {
+        require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+
+    // Create a modifier which only allows a function to be
+    // called if the given proposal's deadline has not been exceeded yet
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadline > block.timestamp,
+            "DEADLINE_EXCEEDED"
+        );
+        _;
+    } 
+
+    // Create a modifier which only allows a function to be
+    // called if the given proposals' deadline HAS been exceeded
+    // and if the proposal has not yet been executed
+    modifier inactiveProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadline <= block.timestamp,
+            "DEADLINE_NOT_EXCEEDED"
+        );
+        require(
+            proposals[proposalIndex].executed == false,
+            "PROPOSAL_ALREADY_EXECUTED"
+        );
+        _;
+    }
     
 }
